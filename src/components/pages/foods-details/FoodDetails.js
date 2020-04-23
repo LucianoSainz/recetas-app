6f0de6248b60b4bbd20799072421d1b33899dd23
luciaoSainz <danielsainz28@gmail.com>
2020-04-23 09:54:11 +0200

    added details

--- src/components/pages/foods-details/FoodDetails.js
+++ src/components/pages/foods-details/FoodDetails.js
@@ -1,10 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 
 class FoodDetails extends Component {
 
+    constructor() {
+        super()
+        this.state = { food: foodsArray[0] }
+    }
+
     render() {
-        return <h1>detalle de alimentos</h1>
+
+        return (
+            <Container>
+                <Row>
+                    <Col md={4}>
+                       <h1>{this.state.food.name}</h1>
+                       <img className="food-datail-img" src={`/img/${this.state.food.img}`}></img>
+                    </Col>
+                </Row>
+            </Container>
+        )
     }
 
 }
